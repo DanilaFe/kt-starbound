88ae0701499374680d9f6657dbaf68163d1d4e60
Danila Fedorin <danila.fedorin@gmail.com>
2017-05-17 16:58:21 -0700

    Format and comment code.

--- src/com/danilafe/ktstarbound/types/world.kt
+++ src/com/danilafe/ktstarbound/types/world.kt
@@ -8,16 +8,37 @@ import java.io.ByteArrayOutputStream
 import java.io.File
 import java.util.zip.Inflater
 
+/**
+ * A class that wraps a Starbound World file.
+ * Currently does not support writing / modifying the world,
+ * due to the relative complexity of a BTree data structure
+ * compared to things like SBVJ.
+ */
 public class World(file: File) {
 
+    /**
+     * The world metadata class, holds information that appears useful to the user
+     * from the inside of the world's Metadata chunk. If the desired info isn't found here,
+     * it can be accessed using the raw bytes and an ArrayReader.
+     */
     public data class WorldMetadata(val worldWidth: Int, val worldHeight: Int, val playerStart: Pair<Double, Double>,
-                             val spawningEnabled: Boolean, val respawnInWorld: Boolean, val adjustPlayerStart: Boolean,
-                             val dungeonIds: MutableMap<Long, String>, val protectedDungeons: MutableList<Long>)
+                                    val spawningEnabled: Boolean, val respawnInWorld: Boolean, val adjustPlayerStart: Boolean,
+                                    val dungeonIds: MutableMap<Long, String>, val protectedDungeons: MutableList<Long>)
 
+    /**
+     * The database that makes up the structure of this file.
+     */
     public val btreedb = BTreeDB5(file)
+    /**
+     * The metadata of the world.
+     */
     public val metadata = readMetadata()
 
-    public fun getKey(layer: Byte, x: Short, y: Short) : ByteArray {
+    /**
+     * Converts the information for lookup in the World
+     * to the key for lookup in the actual BTree.
+     */
+    public fun getKey(layer: Byte, x: Short, y: Short): ByteArray {
         val key = ByteArray(5)
         key[0] = layer
         key[1] = (x.toInt() shr 8).toByte()
@@ -27,7 +48,10 @@ public class World(file: File) {
         return key
     }
 
-    public fun get(layer: Byte, x: Short, y: Short) : ByteArray? {
+    /**
+     * Gets bytes stored using the given data.
+     */
+    public fun get(layer: Byte, x: Short, y: Short): ByteArray? {
         val key = getKey(layer, x, y)
         val data = btreedb.get(key) ?: return null
 
@@ -35,7 +59,7 @@ public class World(file: File) {
         val inflater = Inflater()
         val buffer = ByteArray(2048)
         inflater.setInput(data)
-        while(!inflater.finished()){
+        while (!inflater.finished()) {
             val readBytes = inflater.inflate(buffer)
             output.write(buffer, 0, readBytes)
         }
@@ -43,8 +67,19 @@ public class World(file: File) {
         return output.toByteArray()
     }
 
-    public fun readMetadata() : WorldMetadata? {
-        val data = get(0, 0, 0) ?: return null
+    /**
+     * Reads specifically the bytes used for world metadata storage.
+     */
+    public fun readMetadataBytes(): ByteArray? {
+        return get(0, 0, 0)
+    }
+
+    /**
+     * Generates a new WorldMetadata instance from the metadata bytes
+     * in the BTree.
+     */
+    public fun readMetadata(): WorldMetadata? {
+        val data = readMetadataBytes() ?: return null
         val arrayReader = ArrayReader(data, 0)
         val worldWidth = arrayReader.readInt() ?: return null
         val worldHeight = arrayReader.readInt() ?: return null
@@ -60,7 +95,7 @@ public class World(file: File) {
         val adjustPlayerStart = (worldProperties.data["adjustPlayerStart"] as? DynamicBoolean ?: return null).data
 
         val dungeonMap = mutableMapOf<Long, String>()
-        for(dynamic in (worldProperties.data["dungeonIdMap"] as? DynamicList ?: return null).data){
+        for (dynamic in (worldProperties.data["dungeonIdMap"] as? DynamicList ?: return null).data) {
             val list = dynamic as? DynamicList ?: return null
             dungeonMap.put((list.data[0] as? DynamicVariableInt ?: return null).data,
                     (list.data[1] as? DynamicString ?: return null).data)
@@ -68,7 +103,7 @@ public class World(file: File) {
 
         val protectedDungeons = mutableListOf<Long>()
         (worldProperties.data["protectedDungeonIds"] as? DynamicList ?: return null).data.
-                mapTo(protectedDungeons) { (it as? DynamicVariableInt?: return null).data }
+                mapTo(protectedDungeons) { (it as? DynamicVariableInt ?: return null).data }
 
         return WorldMetadata(worldWidth, worldHeight, playerStartPair,
                 spawningEnabled, respawnInWorld, adjustPlayerStart,
